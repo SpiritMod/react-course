aff3aa35237e807dbe96f086623f5cb1ee8d9f0f
SpiritMod <sashko.harchuk@gmail.com>
2019-10-27 14:00:10 +0200

    lesson-3-task-2

--- src/Components/News/index.js
+++ src/Components/News/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './style.scss';
 
 import { useNews } from './useNews';
@@ -6,18 +6,27 @@ import { Article } from '../Article';
 
 
 export const News = () => {
+  const [loading, setLoading] = useState(false);
 
   const source = useNews();
+
+  useEffect(() => {
+    (source.length > 0) ? setLoading(false) : setLoading(true);
+  }, [source]);
 
   const articles = source.map((item) => {
     return <Article key={item.objectId} {...item}/>
   });
 
+  const loader = <p>Загрузка....</p>;
+
   return (
     <div className="c-news">
       <h2>News</h2>
       <div className="c-news__list">
-        { articles }
+        {
+          loading ? loader : articles
+        }
       </div>
     </div>
   )
