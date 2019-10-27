7dfc54beb12579fd5830f3bc690653d5113570c2
SpiritMod <sashko.harchuk@gmail.com>
2019-10-27 21:20:44 +0200

    lesson-4-task-3

--- src/Components/News/index.js
+++ src/Components/News/index.js
@@ -16,8 +16,13 @@ export const News = () => {
   const history = useHistory();
   const source = useNews();
 
+  const localStorageKey = 'localStoragePosts';
+
 
   useEffect(() => {
+
+    const timerId = setInterval(updateNewsStorage, 5000);
+
     if (id) {
       setData(source.filter(item => {
         return item.objectId === id
@@ -27,6 +32,10 @@ export const News = () => {
     }
 
     (source.length > 0) ? setLoading(false) : setLoading(true);
+
+    return () => {
+      clearInterval(timerId);
+    }
 
   }, [source, id]);
 
@@ -40,6 +49,42 @@ export const News = () => {
   }
 
   const loader = <p>Загрузка....</p>;
+
+  const updateNewsStorage = () => {
+
+    const checkStorage = JSON.parse(localStorage.getItem(localStorageKey));
+
+    const newsStorage = {
+      date: new Date(),
+      source: source
+    };
+
+
+    if (checkStorage) {
+
+      const storageDate = new Date(checkStorage.date).getTime() + (30 * 1000);
+      const currentDate = new Date().getTime();
+
+      if (storageDate < currentDate) {
+        console.log('storageDate < currentDate');
+        localStorage.removeItem(localStorageKey);
+
+        localStorage.setItem(localStorageKey, JSON.stringify({
+          date: new Date(),
+          source: source
+        }));
+
+      } else {
+        console.log('storageDate > currentDate');
+
+      }
+
+    } else {
+      localStorage.setItem(localStorageKey, JSON.stringify(newsStorage));
+    }
+
+    console.log(JSON.parse(localStorage.getItem(localStorageKey)));
+  };
 
   return (
     <div className="o-container">
