e53138af50727cca89d9f1f8421c50d3ba29cdfc
SpiritMod <sashko.harchuk@gmail.com>
2019-11-02 18:52:09 +0200

    lesson-5-task-4

--- src/Assets/InputField/index.js
+++ src/Assets/InputField/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './style.scss'
 import { useField } from 'formik';
 
-export const InputField = ({ label, ...props }) => {
+export const InputField = ({ name, label, error, ...props }) => {
   const [field] = useField({
     ...props
   });
 
   return (
-    <>
+    <div className={ `field ${ error ? "error" : ""}` }>
       <label htmlFor={ props.id || props.name }>{label}</label>
-      <input className='text-input' { ...field } { ...props } />
-    </>
+      <input className="text-input" { ...field } { ...props } />
+      {error && <span className="field-error">{ error }</span>}
+    </div>
   );
 };
--- src/Components/StudentRegistration/index.js
+++ src/Components/StudentRegistration/index.js
@@ -27,11 +27,45 @@ export const StudentRegistration = () => {
     speciality: ""
   };
 
+  const validateFieldFirstName = (values) => {
+    let errors = {};
+
+    if(!values.firstName) {
+      errors.firstName = 'Required';
+    }
+
+    return errors;
+  };
+
+  /*const validateFields = (values) => {
+    const errors = {};
+    if (!values.firstName) {
+      errors.firstName = 'Required';
+    } else if (values.firstName.length > 15) {
+      errors.firstName = 'Must be 15 characters or less';
+    }
+
+    if (!values.lastName) {
+      errors.lastName = 'Required';
+    } else if (values.lastName.length > 20) {
+      errors.lastName = 'Must be 20 characters or less';
+    }
+
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    return errors;
+  };*/
+
   const submitForm = (values) => {
     console.log(values);
     saveLocalStorage(values);
     history.push(book.student);
   };
+
 
   return (
     <>
@@ -41,80 +75,89 @@ export const StudentRegistration = () => {
         <Formik
           initialValues={ getLocalStorage() || initialValues }
           onSubmit={ submitForm }
+          validateOnBlur={false}
+
         >
+          {(props) => {
+            const { errors, touched, isValidating, resetForm, submitForm } = props;
 
-          {props => (
-            <form className="form__registration" onSubmit={props.handleSubmit}>
-              <div className="form__group">
-                <div className="form__field">
-                  <InputField
-                    label="First Name"
-                    name="firstName"
-                    type="text"
-                    placeholder="First Name"
-                    value={props.values.firstName}
-                  />
-                </div>
-                <div className="form__field">
-                  <InputField
-                    label="Surname"
-                    name="surname"
-                    type="text"
-                    placeholder="Surname"
-                    value={props.values.surname}
-                  />
-                </div>
-                <div className="form__field">
-                  <InputField
-                    label="Age"
-                    name="age"
-                    type="number"
-                    placeholder="Age"
-                    value={props.values.age}
-                  />
-                </div>
-                <div className="form__field">
-                  <InputField
-                    label="Email"
-                    name="email"
-                    type="email"
-                    placeholder="Email"
-                    value={props.values.email}
-                  />
-                </div>
+            return (
+              <form className="form__registration" onSubmit={props.handleSubmit}>
+                <div className="form__group">
+                  <div className="form__field">
+                    <InputField
+                      id="firstName"
+                      label="First Name"
+                      name="firstName"
+                      type="text"
+                      placeholder="First Name"
+                      value={ props.values.firstName }
+                      error={ props.errors.firstName }
+                    />
+                  </div>
+                  <div className="form__field">
+                    <InputField
+                      id="surname"
+                      label="Surname"
+                      name="surname"
+                      type="text"
+                      placeholder="Surname"
+                      value={props.values.surname}
+                    />
+                  </div>
+                  <div className="form__field">
+                    <InputField
+                      id="age"
+                      label="Age"
+                      name="age"
+                      type="number"
+                      placeholder="Age"
+                      value={props.values.age}
+                    />
+                  </div>
+                  <div className="form__field">
+                    <InputField
+                      id="email"
+                      label="Email"
+                      name="email"
+                      type="email"
+                      placeholder="Email"
+                      value={props.values.email}
+                    />
+                  </div>
 
-                <div className="form__field">
-                  <FieldRadioGroup title="Sex">
-                    <FieldRadioButton
-                      name="sex"
-                      id="male"
-                      value="male"
-                      checked={props.values.sex === 'male' && 'checked'}
-                    >Male</FieldRadioButton>
-                    <FieldRadioButton
-                      name="sex"
-                      id="female"
-                      value="female"
-                      checked={props.values.sex === 'female' && 'checked'}
-                    >Female</FieldRadioButton>
-                  </FieldRadioGroup>
+                  <div className="form__field">
+                    <FieldRadioGroup title="Sex">
+                      <FieldRadioButton
+                        name="sex"
+                        id="male"
+                        value="male"
+                        checked={props.values.sex === 'male' && 'checked'}
+                      >Male</FieldRadioButton>
+                      <FieldRadioButton
+                        name="sex"
+                        id="female"
+                        value="female"
+                        checked={props.values.sex === 'female' && 'checked'}
+                      >Female</FieldRadioButton>
+                    </FieldRadioGroup>
+                  </div>
+
+                  <SelectField label="Speciality" name="speciality" id="speciality">
+                    <option value="" disabled>Select a speciality</option>
+                    <option value="designer">Designer</option>
+                    <option value="developer">Developer</option>
+                    <option value="manager">Manager</option>
+                  </SelectField>
+
                 </div>
-
-                <SelectField label="Speciality" name="speciality">
-                  <option value="" disabled>Select a speciality</option>
-                  <option value="designer">Designer</option>
-                  <option value="developer">Developer</option>
-                  <option value="manager">Manager</option>
-                </SelectField>
-
-              </div>
-              <div className="form__footer">
-                <Button type="submit">{ getLocalStorage() ? 'Обновить данные' : 'Сохранить данные'}</Button>
-                { (props.submitCount > 0) && <h4 className="form__message">Форма заполнена</h4>}
-              </div>
-            </form>
-          )}
-
+                <div className="form__footer">
+                  <Button type="submit">{ getLocalStorage() ? 'Обновить данные' : 'Сохранить данные'}</Button>
+                  { (props.submitCount > 0) && <h4 className="form__message">Форма заполнена</h4>}
+                </div>
+              </form>
+            )
+          }}
         </Formik>
 
       </div>
